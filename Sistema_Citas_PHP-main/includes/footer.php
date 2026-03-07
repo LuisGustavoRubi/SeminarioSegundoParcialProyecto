2c539072e304cf235f30360d6dde0ae6c19e4d03
juant8560 <microfonos-broncear-0f@icloud.com>
2026-03-07 19:22:58 +0000

    Primer Commit JT

--- Sistema_Citas_PHP-main/includes/footer.php
+++ Sistema_Citas_PHP-main/includes/footer.php
@@ -6,6 +6,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     
+    
     <script>
         function confirmarEliminacion(id, nombre, tipo) {
             Swal.fire({
